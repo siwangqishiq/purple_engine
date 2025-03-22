fc7ea5354733f9f044c448777ff3bb2ab32d14b9
panyi <panyi@yixin.im>
2025-03-22 15:15:31 +0800

    update particle fragment shader

--- assets/shader/particle/particles_frag.glsl
+++ assets/shader/particle/particles_frag.glsl
@@ -1,8 +1,18 @@
 precision highp float;
 
 out vec4 outColor;
+in vec4 particleColor;
 
 void main() {
-    outColor = vec4(0.2f, 0.6f, 1.0f, 1.0f); // 蓝色
+    // outColor = vec4(0.2f, 0.6f, 1.0f, 1.0f); // 蓝色
+    // outColor = particleColor;
+    
+    vec2 center = vec2(0.5f , 0.5f);
+    float distance = length(gl_PointCoord - center);
+    float radius = 0.5;
+
+    float alpha = 1.0 - step(radius, distance);
+    if (alpha <= 0.0) discard;
+    outColor = vec4(particleColor.rgb, alpha);
 }
 
--- assets/shader/particle/particles_vert.glsl
+++ assets/shader/particle/particles_vert.glsl
@@ -8,8 +8,11 @@ layout(std430, binding = 0) buffer SSBO {
     Particle particles[];
 };
 
+out vec4 particleColor;
+
 void main(){
     gl_Position = vec4(particles[gl_VertexID].position, 0.0, 1.0);
-    gl_PointSize = 4.0f;
+    gl_PointSize = 14.0f;
+    particleColor = vec4(1000.0f * (particles[gl_VertexID].velocity), 1.0f , 1.0f);
 }
 
